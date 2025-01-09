7fed938f7ce06e4d337dfa80901c4acd7367d3c6
dpswh-dev <lanutanenzo.developer@gmail.com>
2025-01-09 18:42:28 +0800

    feat(footer): add footer content

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react"
 
+import Footer from "./components/Footer"
+
 function App() {
   const [count, setCount] = useState(2)
 
   return (
     <>
-    <h1 className="text-3xl text-red-500 underline">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl text-red-500 underline">Hello world!</h1>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,24 @@
+export default function Footer() {
+  return (
+    <footer className="text-xl bg-[#1752F0] p-6 space-y-3">
+      <img
+        className="w-auto h-8 mx-auto"
+        src="src/assets/cisco-logo-white.png"
+        alt="White transparent picture of the CISCO logo"
+      />
+      <p className="text-sm text-center text-white">
+        © {new Date().getFullYear()}{" "}
+        <a
+          href="https://github.com/usc-cisco/hermes-site"
+          className="underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hermes Site
+        </a>
+        <br />
+        Developed by the USC Computer and Information Sciences Council. All rights reserved.
+      </p>
+    </footer>
+  )
+}
