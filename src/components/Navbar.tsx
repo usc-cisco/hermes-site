2145d73e38b288930af77bbc233a165baaa8c39d
dpswh-dev <lanutanenzo.developer@gmail.com>
2025-01-09 17:06:33 +0800

    feat(navbar): add navlinks and CISCO logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,26 +10,26 @@ const data = [
 export function Navbar() {
   const [active, setActive] = useState("Billing")
 
-  const links = data.map((item) => (
-    <a
-      className={`${classes.link} text-white`}
-      data-active={item.label === active || undefined}
-      href={item.link}
-      key={item.label}
-      onClick={(event) => {
-        event.preventDefault()
-        setActive(item.label)
-      }}
-    >
-      <span>{item.label}</span>
-    </a>
-  ))
-
   return (
-    <nav className={`flex px-4 py-6 bg-[#1752F0]`}>
+    <nav className={`flex px-6 py-6 bg-[#1752F0]`}>
       <div className={`${classes.navbarMain} flex`}>
         <img className="w-auto h-8 mr-auto" src="src/assets/cisco-logo-white.png" alt="" />
-        <div className="space-x-6">{links}</div>
+        <div className="space-x-6">
+          {data.map((navLink) => (
+            <a
+              className={`${classes.link} text-white font-roboto ring-white focus:ring-1 focus:ring-opacity-75 focus:outline-none p-2`}
+              data-active={navLink.label === active || undefined}
+              href={navLink.link}
+              key={navLink.label}
+              // onClick={(event) => {
+              //   event.preventDefault()
+              //   setActive(navLink.label)
+              // }}
+            >
+              <span>{navLink.label}</span>
+            </a>
+          ))}
+        </div>
       </div>
     </nav>
   )
