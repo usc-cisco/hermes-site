561d787121c4f7ad67f2e2cabfcbed0a53aa11c7
0-BSCode <bryanipsanchez@gmail.com>
2025-01-09 16:57:55 +0800

    fix(app): change counter state

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
 
 function App() {
-  const [count, setCount] = useState(69)
+  const [count, setCount] = useState(0)
 
   return (
     <>
