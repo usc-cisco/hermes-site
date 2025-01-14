2e8049f0423b54456aa944d3e2fbd92b24eaf0f0
0-BSCode <bryanipsanchez@gmail.com>
2025-01-14 10:27:40 +0800

    fix(announcements): add announcement about physical queue

--- src/types/constants/announcements.ts
+++ src/types/constants/announcements.ts
@@ -6,6 +6,7 @@ export const announcements: Announcement[] = [
     date: new Date("2025-01-14"),
     points: [
       "Adjustment requests to only change schedule will not be catered to today. Only requests to add or remove classes will be catered to.",
+      "The physical queue will take priority over the digital queue. Please do not use the digital queue if you are already in the physical queue.",
     ],
   },
 ]
