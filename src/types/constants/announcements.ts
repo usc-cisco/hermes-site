bcf5eef659be336bf0913cd780d764ddff08a2b9
Zeus D. Elderfield <elderfieldzeus24@gmail.com>
2025-08-04 13:42:15 +0800

    Feat/new announcements (#88)
    * refactor(faq): change is coordinator
    
    * refactor: updated coordinator for 1st sem
    
    * feat(announcement): added mme roa's announcement

--- src/types/constants/announcements.ts
+++ src/types/constants/announcements.ts
@@ -3,10 +3,9 @@ import { Announcement } from "../entities/Announcement"
 export const announcements: Announcement[] = [
   {
     id: 1,
-    date: new Date("2025-01-14"),
+    date: new Date("2025-08-01"),
     points: [
-      "Adjustment requests to only change schedule will not be catered to at all. Only requests to add or remove classes will be catered to.",
-      "The physical queue will take priority over the digital queue. Please do not use the digital queue if you are already in the physical queue.",
+      "BSCS STUDENTS: Starting this semester, a No Change Policy will be strictly enforced during the adjustment period. Changes to block sections, GE courses, group numbers, subjects, and schedules will no longer be allowed. Only enrollment of new or replacement subjects (for dissolved ones) will be accommodated. Finalize your schedules during the non-block and open market phases.",
     ],
   },
 ]
