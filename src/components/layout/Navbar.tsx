9c53958b2d05ba8c203faf52ee753614fb8ed28b
realjeeyo <jra.ldelgado@gmail.com>
2026-01-23 23:01:25 +0800

    style(navbar.tsx): navbar styling change

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -7,16 +7,18 @@ export function Navbar() {
   const { isAuthenticated, isAdmin, clearAuth } = useAuth()
 
   return (
-    <nav className="mb-4 bg-primary px-6 py-4">
+    <nav className="relative z-10 mb-4 px-6 py-4">
       <div className="flex items-center gap-2">
-        <Link to="/" className="flex items-center text-lg font-semibold text-white sm:text-xl">
-          <img src="/logo-white.svg" alt="Hermes Logo" className="mr-2 inline h-6 w-auto" />
-          HERMES
-        </Link>
-        <span className="text-sm text-gray-400">| queue.dcism.org</span>
+        <div className="flex items-center gap-2 rounded-full bg-black px-6 py-2">
+          <Link to="/" className="flex items-center text-lg font-semibold text-white sm:text-xl">
+            <img src="/logo-white.svg" alt="Hermes Logo" className="mr-2 inline h-6 w-auto" />
+            HERMES
+          </Link>
+          <span className="text-sm text-gray-300">| queue.dcism.org</span>
+        </div>
         <div className="ml-auto flex items-center gap-2 space-x-6">
           <Link
-            className="rounded-md leading-none text-white ring-white focus:outline-none focus:ring-1 focus:ring-opacity-75"
+            className="rounded-full bg-black px-6 py-3 leading-none text-white ring-white focus:outline-none focus:ring-1 focus:ring-opacity-75"
             to="/faq"
           >
             FAQs
